dc9bf59896c00506a727db08c9e8d620d56ef85e
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-22 04:47:30 +0100

    Work with basic types

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
-const userName = 'Miracle';
+function displayProfile(name: string, age: number, showAge: boolean) {
+  console.log(`My name is ${name}`);
+  if (showAge) {
+    console.log(`I am ${age} years old.`);
+  }
+}
 
-console.log(userName);
+const myName = 'Sixtus';
+const age = new Date().getFullYear() - 2003;
+
+displayProfile(myName, age, true);

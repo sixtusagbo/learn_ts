5a236145ea59bcda2387831fe9bf548503e19ba4
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-22 05:59:08 +0100

    Use tuples

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // tuple
+} = {
   name: 'Sixtus',
   age: 20,
   hobbies: ['Basketball', 'Music', 'Football'],
+  role: [1, 'admin'],
 };
+
+person.role.push('user'); // push() is an exception
+// person.role[1] = 10; // Type 'number' is not assignable to type 'string'
+// person.role = [3, 'author', 'something else']; //! ERROR
 
 let favouriteActivities: any[];
 favouriteActivities = ['Sports', 3];
@@ -10,3 +20,5 @@ favouriteActivities = ['Sports', 3];
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
+
+console.log(person);

511ef472aa8df3a40c5c56b77bc683d2f2b36b9b
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-22 04:47:30 +0100

    Work with basic types

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+function displayProfile(name: string, age: number, showAge: boolean) {
+  console.log(`My name is ${name}`);
+  if (showAge) {
+    console.log(`I am ${age} years old.`);
+  }
+}
+
+const myName = 'Sixtus';
+const age = new Date().getFullYear()  - 2003;
+
+
+displayProfile(myName, age, true);

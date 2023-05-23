2c0c5b7e08c7434468cff8aa34f99380cdd8c2fc
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-23 14:16:48 +0100

    Debug with chrome

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 const userName = 'sixtusagbo';
 
 console.log(userName);
+
+function add(n1: number, n2: number) {
+  const sum = n1 + n2;
+  if (sum > 0) {
+    return sum;
+  }
+  return;
+}
+
+add(3, 6);

f8f4dabd3b250f00577bd8b091699088e14259ba
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-24 12:42:23 +0100

    Make use of Inheritance

--- ts-compiler/src/app.ts
+++ ts-compiler/src/app.ts
@@ -22,6 +22,33 @@ class Department {
   }
 }
 
+class ITDepartment extends Department {
+  constructor(id: string, private admins: string[]) {
+    super(id, 'IT');
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[] = []) {
+    super(id, 'Accounting');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  displayReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('it003', ['Sixtus']);
+const accounting = new AccountingDepartment('acc001');
+accounting.addReport('New ticket...');
+
+console.log(it);
+console.log(accounting);
+
 const computerScience = new Department('csc001', 'Computer Science');
 computerScience.addEmployee('Sixtus');
 computerScience.addEmployee('Mark');

51a5bae438f86d8e958bc06523bb5ebbf2ab6224
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-24 10:52:40 +0100

    Clean up

--- ts-compiler/src/app.ts
+++ ts-compiler/src/app.ts
@@ -1,9 +1,1 @@
-const addNumbers = (...numbers: number[]) =>
-  numbers.reduce((curResult, curValue) => curResult + curValue, 0);
-
-// With tuples
-const addThreeNumbers = (...numbers: [number, number, number]) =>
-  numbers.reduce((curResult, curValue) => curResult + curValue, 0);
-
-console.log(addThreeNumbers(3, 4, 5));
-console.log(addNumbers(3, 4, 5, 56, 67, 2, 1));
+console.log('Here I go!');

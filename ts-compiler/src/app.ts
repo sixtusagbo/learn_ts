403209242b23cf4b6c89abcc6e820ac6028af8f3
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-24 11:09:21 +0100

    Use constructors and this keyword

--- ts-compiler/src/app.ts
+++ ts-compiler/src/app.ts
@@ -1,1 +1,15 @@
-console.log('Here I go!');
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  describe() {
+    console.log('Department: ' + this.name);
+  }
+}
+
+const computerScience = new Department('Computer Science');
+
+console.log(computerScience.describe());

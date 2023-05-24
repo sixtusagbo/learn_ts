dbe4d2539a044caef6f926657720e566dfe1dcce
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-24 11:22:38 +0100

    Use private and public access modifiers

--- ts-compiler/src/app.ts
+++ ts-compiler/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -8,8 +9,23 @@ class Department {
   describe() {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    // validation, etc
+    this.employees.push(employee);
+  }
+
+  employeeInfo() {
+    console.log(`There are ${this.employees.length} employees:`);
+    console.log(this.employees);
+  }
 }
 
 const computerScience = new Department('Computer Science');
+computerScience.addEmployee('Sixtus');
+computerScience.addEmployee('Mark');
 
-console.log(computerScience.describe());
+// computerScience.employees[2] = 'Isaac'; //! Error
+
+computerScience.describe();
+computerScience.employeeInfo();

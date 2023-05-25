675b6aa01904695543ad37da9620b6632db89732
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-25 18:14:43 -0500

    Use type casting

--- advanced_types/src/app.ts
+++ advanced_types/src/app.ts
@@ -1,28 +1,4 @@
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
-}
+// const userInput = <HTMLInputElement>document.getElementById('userInput')!; // alternative
+const userInput = document.getElementById('userInput') as HTMLInputElement; // I prefer this
 
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-    case 'horse':
-      speed = animal.runningSpeed;
-      break;
-    default:
-      break;
-  }
-  console.log('Moving with speed: ' + speed);
-}
-
-moveAnimal({ type: 'bird', flyingSpeed: 7 });
+userInput.value = 'Hi, there!';
---
+++ advanced_types/src/discriminated_unions.ts
@@ -0,0 +1,28 @@
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+    default:
+      break;
+  }
+  console.log('Moving with speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 7 });

ef0b1601e84808fe7160ff5046f264cf594e4fea
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-26 07:17:05 -0500

    Add other kinds of decorators

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -41,7 +41,7 @@ class Person {
 
 /**
  * Executes when the class definition is registered by JS
- * @param target prototype
+ * @param target prototype(instance) or constructor(static)
  * @param propertyName name of the property
  */
 function Log(target: any, propertyName: string | Symbol) {
@@ -49,11 +49,37 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -67,7 +93,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }

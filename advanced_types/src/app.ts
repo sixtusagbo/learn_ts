94413d2e7aa50fef24362bb0bc5a089b0e670ec8
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-25 19:12:10 -0500

    Use optional chaining

--- advanced_types/src/app.ts
+++ advanced_types/src/app.ts
@@ -1,20 +1,9 @@
-type Combinable = string | number;
-type Numeric = number | boolean;
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Sixtus',
+  others: { gender: 'Male', active: true },
+};
 
-type Universal = Combinable & Numeric;
-
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string') {
-    return a.toString() + b.toString();
-  }
-  return a + b;
-}
-
-const result = add('Sixtus', ' Agbo');
-const result2 = result.split(' ');
-console.log(result);
-console.log(result2);
+// optional chaining
+// compiled to if checks
+console.log(fetchedUserData?.others?.gender);
---
+++ advanced_types/src/function_overloads.ts
@@ -0,0 +1,20 @@
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add('Sixtus', ' Agbo');
+const result2 = result.split(' ');
+console.log(result);
+console.log(result2);

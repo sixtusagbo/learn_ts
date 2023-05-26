e81b2f95dff2e7451f62a118f20f022c32a576cc
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-25 23:00:24 -0500

    Create custom Generic function

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,12 +1,6 @@
-const names: Array<string> = ['Sixtus Agbo', 'Joseph Agbo']; // string[]
-// names[0].split(' ');
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  }, 2000);
-});
-
-promise.then(data => {
-  data.split(' ');
-});
+const mergedObj = merge({ name: 'Sixtus' }, { gender: 'Male' });
+console.log(mergedObj);

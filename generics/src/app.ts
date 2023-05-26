c3fe052178856d0044d8dea28a6be436ac3cfd52
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-25 23:14:42 -0500

    Add custom generic functions

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -4,3 +4,21 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: 'Sixtus' }, { gender: 'Male' });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = 'Got no value';
+  const count = element.length;
+
+  if (count === 1) {
+    description = 'Got 1 element';
+  } else if (count > 1) {
+    description = 'Got ' + count + ' elements';
+  }
+  return [element, description];
+}
+
+console.log(countAndDescribe('Hello World!'));

ff984407767ce4c46b4e2c86c4fee83818475c2f
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-28 04:25:45 +0100

    Use declare keyword

--- third-party-libs/src/app.ts
+++ third-party-libs/src/app.ts
@@ -1,3 +1,10 @@
 import _ from 'lodash';
 
+/**
+ * The declare keyword lets typescript know that this will be available
+ */
+declare var SOMEGLOBAL: any;
+
 console.log(_.shuffle([1, 2, 3]));
+
+console.log(SOMEGLOBAL);

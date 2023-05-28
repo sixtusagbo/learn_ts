c08c7eec5f2b3a712213b05e4344ad5cfd9523a3
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-28 06:45:44 +0100

    Use class-transformer and class-validator

--- third-party-libs/src/app.ts
+++ third-party-libs/src/app.ts
@@ -1,10 +1,29 @@
-import _ from 'lodash';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import 'reflect-metadata';
 
-/**
- * The declare keyword lets typescript know that this will be available
- */
-declare var SOMEGLOBAL: any;
+import Product from './product.model';
 
-console.log(_.shuffle([1, 2, 3]));
+/** mock data from server */
+const products = [
+  { title: 'Pen', price: 29.99 },
+  { title: 'Pencil', price: 10.99 },
+];
+const prod = new Product('', -5.99);
+validate(prod).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+    console.log(prod.getInformation());
+  }
+});
 
-console.log(SOMEGLOBAL);
+// const loadedProducts = products.map(
+//   product => new Product(product.title, product.price)
+// );
+const loadedProducts = plainToInstance(Product, products);
+
+for (const product of loadedProducts) {
+  console.log(product.getInformation());
+}

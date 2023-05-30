1540d00265f74b74ad67108b8eab491dae2674fe
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-30 05:28:00 +0100

    Add todolist component

--- todo_with_react_typescript/src/App.tsx
+++ todo_with_react_typescript/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import TodoLists from './components/TodoList';
+
 const App: React.FC = () => {
+  const todos = [
+    { id: 't1', text: 'Finish Dart 3 codelab' },
+    { id: 't2', text: 'Attend lectures today' },
+    { id: 't3', text: 'Have breakfast before leaving!' },
+  ];
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <TodoLists items={todos} />
     </div>
   );
 };
